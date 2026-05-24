b3f0a618653b1e9767a53213983d24c96018aba9
germaniuss <germanchik01atalyants@gmail.com>
2026-05-24 18:18:07 +0200

    ♻️ zb: Move UnixSocket connection code to unix.rs
    Move UnixSocket connection code to unix.rs from mod.rs
    for better readability and maintainability.

--- zbus/src/address/transport/unix.rs
+++ zbus/src/address/transport/unix.rs
@@ -1,3 +1,6 @@
+#[cfg(any(unix, not(feature = "tokio")))]
+use crate::{Error, Result};
+
 #[cfg(target_os = "linux")]
 use std::ffi::OsString;
 use std::{
@@ -8,6 +11,16 @@ use std::{
 
 #[cfg(unix)]
 use super::encode_percents;
+
+#[cfg(not(feature = "tokio"))]
+use async_io::Async;
+
+#[cfg(target_os = "linux")]
+use std::os::linux::net::SocketAddrExt;
+#[cfg(unix)]
+use std::os::unix::net::{SocketAddr, UnixStream};
+#[cfg(all(windows, not(feature = "tokio")))]
+use uds_windows::UnixStream;
 
 /// A Unix domain socket transport in a D-Bus address.
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -54,6 +67,68 @@ impl Unix {
         };
 
         Ok(Self::new(path))
+    }
+
+    #[cfg(not(feature = "tokio"))]
+    pub(super) async fn connect(self) -> Result<Async<UnixStream>> {
+        let stream = self.get_stream().await?;
+        Async::new(stream).map_err(|e| Error::InputOutput(e.into()))
+    }
+
+    #[cfg(all(unix, feature = "tokio"))]
+    pub(super) async fn connect(self) -> Result<tokio::net::UnixStream> {
+        let stream = self.get_stream().await?;
+        tokio::net::UnixStream::from_std(stream).map_err(|e| Error::InputOutput(e.into()))
+    }
+
+    #[cfg(any(unix, not(feature = "tokio")))]
+    async fn get_stream(self) -> Result<UnixStream> {
+        #[cfg(unix)]
+        let addr = self.take_socket_addr()?;
+        #[cfg(windows)]
+        let addr = self.take_path_addr()?;
+
+        crate::Task::spawn_blocking(
+            move || -> Result<_> {
+                #[cfg(unix)]
+                let stream = UnixStream::connect_addr(&addr)?;
+                #[cfg(windows)]
+                let stream = UnixStream::connect(addr)?;
+                stream.set_nonblocking(true)?;
+
+                Ok(stream)
+            },
+            "unix stream connection",
+        )
+        .await?
+    }
+
+    #[cfg(all(windows, not(feature = "tokio")))]
+    fn take_path_addr(self) -> Result<PathBuf> {
+        // This is a `path` in case of Windows until uds_windows provides the needed API:
+        // https://github.com/haraldh/rust_uds_windows/issues/14
+        match self.take_path() {
+            UnixSocket::File(path) => Ok(path),
+            UnixSocket::Dir(_) | UnixSocket::TmpDir(_) => {
+                // You can't connect to a unix:dir.
+                Err(Error::Unsupported)
+            }
+        }
+    }
+
+    #[cfg(unix)]
+    fn take_socket_addr(self) -> Result<SocketAddr> {
+        match self.take_path() {
+            UnixSocket::File(path) => Ok(SocketAddr::from_pathname(path)?),
+            #[cfg(target_os = "linux")]
+            UnixSocket::Abstract(name) => {
+                Ok(SocketAddr::from_abstract_name(name.as_encoded_bytes())?)
+            }
+            UnixSocket::Dir(_) | UnixSocket::TmpDir(_) => {
+                // You can't connect to a unix:dir.
+                Err(Error::Unsupported)
+            }
+        }
     }
 }
 
